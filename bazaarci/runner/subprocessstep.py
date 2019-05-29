36d36eddd37e557f8da9fc53485c1dcdd2d6ce40
Dan Svoboda <dansvo@DESKTOP-8FRSCM4.localdomain>
2019-05-29 09:26:35 -0400

    propose method for grabbing returned CompletedProcess object for SubprocessStep

--- bazaarci/runner/subprocessstep.py
+++ bazaarci/runner/subprocessstep.py
@@ -7,8 +7,12 @@ from bazaarci.runner import Step
 
 class SubprocessStep(Step):
     def __init__(self, name, graph: Optional["Graph"], *sp_args, **sp_kwargs):
+        self.returned = None
         super().__init__(
             name,
             graph,
-            target=partial(subprocess.run, *sp_args, **sp_kwargs),
+            target=self.run_grabbing_returned(*sp_args, **sp_kwargs),
         )
+
+    def run_grabbing_returned(self, *sp_args, **sp_kwargs):
+        self.returned = subprocess.run(*sp_args, **sp_kwargs)
